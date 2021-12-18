e8b5d02ca8457246af0751623098bf7d2df4ad2e
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-12-18 21:59:21 +0300

    Update

--- myScoreApp/Startup.cs
+++ myScoreApp/Startup.cs
@@ -22,12 +22,10 @@ namespace myScoreApp
             Configuration = configuration;
         }
         public IConfiguration Configuration { get; }
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
