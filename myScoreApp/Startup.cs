cac1022353eb66c7ee0bfd693a0cd6295c31a460
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-12-19 23:05:31 +0300

    Update

--- myScoreApp/Startup.cs
+++ myScoreApp/Startup.cs
@@ -52,7 +52,6 @@ namespace myScoreApp
             });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseAuthentication();
